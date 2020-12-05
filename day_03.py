023d51ea5b08f8ec765a905e9a800b84a1765aef
Fernando Farfan <fernando.farfan@thermofisher.com>
2020-12-04 21:25:04 -0500

    Day 3: Toboggan Trajectory

---
+++ day_03.py
@@ -0,0 +1,143 @@
+# https://adventofcode.com/2020/day/3
+# --- Day 3: Toboggan Trajectory ---
+#
+# With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy,
+# it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which
+# angles will take you near the fewest trees.
+#
+# Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your
+# puzzle input) of the open squares (.) and trees (#) you can see. For example:
+#
+# ..##.......
+# #...#...#..
+# .#....#..#.
+# ..#.#...#.#
+# .#...##..#.
+# ..#.##.....
+# .#.#.#....#
+# .#........#
+# #.##...#...
+# #...##....#
+# .#..#...#.#
+#
+# These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome
+# stability, the same pattern repeats to the right many times:
+#
+# ..##.........##.........##.........##.........##.........##.......  --->
+# #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+# .#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+# ..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+# .#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+# ..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+# .#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+# .#........#.#........#.#........#.#........#.#........#.#........#
+# #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+# #...##....##...##....##...##....##...##....##...##....##...##....#
+# .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+#
+# You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on
+# your map).
+#
+# The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers);
+# start by counting all the trees you would encounter for the slope right 3, down 1:
+#
+# From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position
+# that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+#
+# The locations you'd check in the above example are marked here with O where there was an open square and X where there
+# was a tree:
+#
+# ..##.........##.........##.........##.........##.........##.......  --->
+# #..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+# .#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+# ..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+# .#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+# ..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+# .#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+# .#........#.#........X.#........#.#........#.#........#.#........#
+# #.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+# #...##....##...##....##...#X....##...##....##...##....##...##....#
+# .#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+#
+# In this example, traversing the map using this slope would cause you to encounter 7 trees.
+#
+# Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you
+# encounter?
+#
+# Your puzzle answer was 162.
+#
+# --- Part Two ---
+#
+# Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+#
+# Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left
+# corner and traverse the map all the way to the bottom:
+#
+#     Right 1, down 1.
+#     Right 3, down 1. (This is the slope you already checked.)
+#     Right 5, down 1.
+#     Right 7, down 1.
+#     Right 1, down 2.
+#
+# In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these
+# produce the answer 336.
+#
+# What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+#
+# Your puzzle answer was 3064612320.
+#
+
+TREE = '#'
+
+
+def puzzle_1(the_map, right_increment, down_increment):
+
+    curr_x = 1
+    curr_y = 1
+
+    end_x = len(the_map[0])
+    end_y = len(the_map)
+
+    trees_found = 0
+
+    while curr_y <= end_y:
+        if the_map[curr_y - 1][curr_x - 1] == TREE:
+            trees_found += 1
+
+        curr_x = move_right(curr_x, end_x, right_increment)
+        curr_y = move_down(curr_y, down_increment)
+
+    return trees_found
+
+
+def puzzle_2(the_map):
+    r1_d1 = puzzle_1(the_map, 1, 1)
+    r3_d1 = puzzle_1(the_map, 3, 1)
+    r5_d1 = puzzle_1(the_map, 5, 1)
+    r7_d1 = puzzle_1(the_map, 7, 1)
+    r1_d2 = puzzle_1(the_map, 1, 2)
+
+    print(r1_d1 * r3_d1 * r5_d1 * r7_d1 * r1_d2)
+
+
+def move_right(curr_x, end_x, right_increment):
+    if curr_x + right_increment > end_x:
+        steps = (curr_x + right_increment) - end_x
+        return steps
+
+    return curr_x + right_increment
+
+
+def move_down(curr_y, down_increment):
+    return curr_y + down_increment
+
+
+if __name__ == '__main__':
+    the_map = []
+    # with open('test_03.txt', 'r') as f_input:
+    with open('inputs/input_03.txt', 'r') as f_input:
+        the_map = [line.strip() for line in f_input.readlines()]
+
+    print(puzzle_1(the_map, right_increment=3, down_increment=1))
+
+    puzzle_2(the_map)
